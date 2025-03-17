0aa7b054bc9bb7472274675b9ae4ef17f4d2f21f
ann <annagotina3001@gmail.com>
2025-03-17 10:42:07 +0300

    Первый коммит

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Firma
+{
+    // Поля класса
+    public string Name { get; set; }      // Название фирмы
+    public string Address { get; set; }   // Адрес фирмы
+    public int FoundationYear { get; set; } // Год основания фирмы
+
+    // Конструктор
+    public Firma(string name, string address, int foundationYear)
+    {
+        Name = name;
+        Address = address;
+        FoundationYear = foundationYear;
+    }
+
+    // Метод для вывода информации о фирме
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Название фирмы: {Name}");
+        Console.WriteLine($"Адрес: {Address}");
+        Console.WriteLine($"Год основания: {FoundationYear}");
+    }
+}
